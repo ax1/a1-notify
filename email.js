35e70c39e18091588bb07c5a6c637502b5dcdf95
Angel Rego <angelrego@gmail.com>
2022-03-02 19:05:07 +0100

    email: new env variable names

--- email.js
+++ email.js
@@ -6,9 +6,9 @@ module.exports = { send }
 const { createTransport } = require('nodemailer')
 
 async function send(to, subject, body) {
-  const user = process.env.USER_EMAIL
-  const pass = process.env.USER_APP_PW
-  if (!user && !pass) throw Error('Add USER_EMAIL and USER_APP_PW env variables before sending email')
+  const user = process.env.EMAIL_USER
+  const pass = process.env.EMAIL_APP_PW
+  if (!user && !pass) throw Error('Add EMAIL_USER and EMAIL_APP_PW env variables before sending email')
   const mail = createTransport({ host: 'smtp.gmail.com', port: 465, secure: true, auth: { user, pass },/*logger: true, debug: true*/ })
   const params = { from: user, to, subject }
   body.startsWith('<') ? params.html = body : params.text = body
